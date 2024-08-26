7f862135983fc4faab8e7d7edc62d041c3699aa8
Stefan Verbruggen <stefan.verbruggen@asml.com>
2024-08-26 20:51:08 +0200

    test DeltaLakePyarrowIOManager instead of polars one

--- libraries/dagster-delta/dagster_delta_tests/test_iomanager.py
+++ libraries/dagster-delta/dagster_delta_tests/test_iomanager.py
@@ -1,37 +1,40 @@
 import os
 
-import polars as pl
+import pyarrow as pa
+import pyarrow.compute as pc
 import pytest
 from dagster import (
     Out,
     graph,
     op,
 )
-from dagster_delta_polars import DeltaLakePolarsIOManager
 from deltalake import DeltaTable
 
-from dagster_delta import LocalConfig
+from dagster_delta import DeltaLakePyarrowIOManager, LocalConfig
 from dagster_delta.io_manager import WriteMode
 
 
 @op(out=Out(metadata={"schema": "a_df"}))
-def a_df() -> pl.DataFrame:
-    return pl.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+def a_pa_table() -> pa.Table:
+    return pa.Table.from_pydict({"a": [1, 2, 3], "b": [4, 5, 6]})
 
 
 @op(out=Out(metadata={"schema": "add_one"}))
-def add_one(df: pl.DataFrame) -> pl.DataFrame:
-    return df + 1
+def add_one(table: pa.Table) -> pa.Table:
+    # Add one to each column in the table
+    updated_columns = [pc.add_checked(table[column], pa.scalar(1)) for column in table.column_names]  # type: ignore
+    updated_table = pa.Table.from_arrays(updated_columns, names=table.column_names)
+    return updated_table
 
 
 @graph
-def add_one_to_dataframe():
-    add_one(a_df())
+def add_one_to_dataset():
+    add_one(a_pa_table())
 
 
 @pytest.fixture()
-def io_manager_with_parquet_read_options(tmp_path) -> DeltaLakePolarsIOManager:
-    return DeltaLakePolarsIOManager(
+def io_manager_with_parquet_read_options(tmp_path) -> DeltaLakePyarrowIOManager:
+    return DeltaLakePyarrowIOManager(
         root_uri=str(tmp_path),
         storage_options=LocalConfig(),
         mode=WriteMode.overwrite,
@@ -45,7 +48,7 @@ def test_deltalake_io_manager_with_parquet_read_options(
 ):
     resource_defs = {"io_manager": io_manager_with_parquet_read_options}
 
-    job = add_one_to_dataframe.to_job(resource_defs=resource_defs)
+    job = add_one_to_dataset.to_job(resource_defs=resource_defs)
 
     # run the job twice to ensure that tables get properly deleted
     for _ in range(2):
